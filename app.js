989a289b3c75868d3ef703e6ee304c56737339c4
KrishnaTyagi01 <tyagikrishna38@gmail.com>
2022-12-04 18:21:56 +0530

    minor

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
-const PORT =process.env.PORT || 80;
+const PORT =process.env.PORT || 8000;
 const mongoose = require('mongoose');
 
 const { MONGOURI } = require('./config/keys');
+
+require('./models/user');
+require('./models/post');
+
+app.use(express.json());
+app.use(require('./routes/auth'));
+app.use(require('./routes/post'));
+app.use(require('./routes/user'));
 
 mongoose.connect(MONGOURI, {
     useUnifiedTopology: true,
@@ -16,23 +24,6 @@ mongoose.connection.on('connected', () => {
 mongoose.connection.on('error', (err) => {
     console.log('Connection error', err);
 });
-
-require('./models/user');
-require('./models/post');
-
-app.use(express.json());
-app.use(require('./routes/auth'));
-app.use(require('./routes/post'));
-app.use(require('./routes/user'));
-
-
-if(process.env.NODE_ENV == 'production'){
-    app.use(express.static('client/build'))
-    const path = require('path')
-    app.get("*", (req, res)=>{
-        res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'))
-    })
-}
 
 app.listen(PORT, () => {
     console.log(`App running at port ${PORT}`);
